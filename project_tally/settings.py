dbe01607dc3bb2332b1707cbb4c22b8ff23bd8be
Vasundraa <vasundraas@gmail.com>
2024-04-27 15:23:43 +0530

    Your commit message here

--- project_tally/settings.py
+++ project_tally/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,9 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
-django_heroku
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
