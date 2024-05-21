9f3ace4e7caee4dcc7821346083a2e697457b2c9
banusrisathyan <168272354+banusrisathyan@users.noreply.github.com>
2024-05-21 19:44:31 +0530

    Update settings.py

--- project_tally/settings.py
+++ project_tally/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-57h$zgur$9=)d8=vzl*tg#vgs(w)l)_m406%%6342b%tge2q9o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
