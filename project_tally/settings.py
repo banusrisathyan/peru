9c630016229a1339e0d01bc4bb92bd76dae75d36
banusrisathyan <168272354+banusrisathyan@users.noreply.github.com>
2024-06-04 15:37:33 +0530

    Update settings.py

--- project_tally/settings.py
+++ project_tally/settings.py
@@ -88,12 +88,14 @@ WSGI_APPLICATION = 'project_tally.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',  # Change this if your database is hosted elsewhere
-        'PORT': '5432',  # Leave empty to use the default PostgreSQL port (5432)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'postgres',
+        #'USER': 'postgres',
+        #'PASSWORD': 'admin',
+        #'HOST': 'localhost',  # Change this if your database is hosted elsewhere
+        #'PORT': '5432',  # Leave empty to use the default PostgreSQL port (5432)
     }
 }
 
